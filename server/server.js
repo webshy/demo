fc82fcb1756bc1c3e5517fa6c8903721dcb9c2c3
hsy <haungshaoyuan@shuziguanxing.com>
2020-12-18 17:25:11 +0800

    增加

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const Mock = require('mockjs')
+const app = express()
+const Random = Mock.Random
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next()
+})
+
+// 获取文章列表
+app.get('/posts', (req, res) => {
+  let number = 100
+  let posts = []
+  for (let i = 0; i < number; i++) {
+    posts.push(Mock.mock({
+      id: i,
+      title: Random.cparagraph(1),
+      content: Random.cparagraph(2, 5),
+      time: Random.datetime('yyy-MMM-ddd hh:mm:ss'),
+      author: Random.cname(),
+      'like|1-10001': 1
+    }))
+    posts.push(i)
+  }
+
+  // 获取当前页码 每页显示条数 计算总数页
+  let perPageNumber = Number(req.query.perPageNumber ? req.query.perPageNumber : 10)
+  let currentPage = Number(req.query.currentPage ? req.query.currentPage : 1)
+  let totalPage = Math.ceil(posts.length / perPageNumber)
+
+  let start = (currentPage - 1) * perPageNumber
+  let end = currentPage * perPageNumber <= posts.length ? currentPage * perPageNumber : posts.length
+  posts = posts.slice(start, end)
+
+  res.json({ content: posts, currentPage, totalPage })
+  res.json(posts)
+
+})
+
+
+
+  
--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [posts, setPosts] = useState([])
+  const [Loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPage, setTotalPage] = useState(1)
+  const [perPageNumber, setPerPageNumber] = useState(10)
+
+  useEffect(() => {
+    const requetposts = async () => {
+      setLoading(true)
+      const response = await axios.get('http://localhost:3001/posts',{ currentPage , perPageNumber })
+      console.log(response)
+      setPosts(response.data.content)
+      setTotalPage(response.data.totalPage)
+      setLoading(false)
+    }
+    requetposts()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   My App
+    // </div>
+    <div className="container">
+      <h1 className="my-project-title">我的文章</h1>
     </div>
   );
 }
